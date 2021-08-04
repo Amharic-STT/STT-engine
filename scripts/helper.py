507f5fbbb6cf7f89eef962575d1ec8f67333faf1
Daniel <danielzelalemheru@gmail.com>
2021-08-04 14:31:09 +0300

    refactor data preprocessing and data loader

---
+++ scripts/helper.py
@@ -0,0 +1,31 @@
+import pickle
+
+
+def read_obj(path):
+    with open(path, "rb") as f:
+        return pickle.load(f)
+
+
+def write_obj(path, obj):
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
+
+
+def read_csv(csv_path, missing_values=[]):
+    try:
+        df = pd.read_csv(csv_path, na_values=missing_values)
+        print("file read as csv")
+        return df
+    except FileNotFoundError:
+        print("file not found")
+
+
+def save_csv(df, csv_path):
+    try:
+        df.to_csv(csv_path, index=False)
+        print('File Successfully Saved.!!!')
+
+    except Exception:
+        print("Save failed...")
+
+    return df
