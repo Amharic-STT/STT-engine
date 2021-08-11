1b2e776edbbe1ff92669381871c1887394c5fbd2
Azariagmt <azariagebremichael10@gmail.com>
2021-08-11 18:32:34 +0300

    setsup dvc pipeline

--- scripts/train.py
+++ scripts/train.py
@@ -1,3 +1,4 @@
+import math
 import sys
 
 from dataset_loader import load_audio_files, load_transcripts, load_spectrograms_with_transcripts, load_spectrograms_with_transcripts_in_batches
@@ -6,7 +7,7 @@ from FeatureExtraction import FeatureExtraction
 from transcript_encoder import fit_label_encoder, encode_transcripts, decode_predicted
 from models import model_1, model_2, model_3
 
-import librosa   #for audio processing
+import librosa  # for audio processing
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,11 +16,13 @@ import os
 import mlflow
 import mlflow.keras
 import logging
-len(os.listdir('../data/train/wav/'))
+import json
+len(os.listdir('./data/train/wav/'))
 
 sample_rate = 8000
 
-audio_files, maximum_length = load_audio_files('../data/train/wav/', sample_rate, True)
+audio_files, maximum_length = load_audio_files(
+    './data/train/wav/', sample_rate, True)
 logging.info('loaded audio files')
 
 print("The longest audio is", maximum_length/sample_rate, 'seconds long')
@@ -27,7 +30,7 @@ print("max length", maximum_length)
 
 demo_audio = list(audio_files.keys())[0]
 
-transcripts = load_transcripts("../data/train/trsTrain.txt")
+transcripts = load_transcripts("./data/train/trsTrain.txt")
 logging.info('loaded transcripts')
 
 audio_files = resize_audios_mono(audio_files, maximum_length)
@@ -39,9 +42,11 @@ print("augmented shape", audio_files[demo_audio].shape)
 feature_extractor = FeatureExtraction()
 mfcc_features = feature_extractor.extract_features(audio_files, sample_rate)
 
-feature_extractor.save_mfcc_spectrograms(mfcc_features, sample_rate, '../data/train/mfcc_spectros/')
+feature_extractor.save_mfcc_spectrograms(
+    mfcc_features, sample_rate, './data/train/mfcc_spectros/')
 print('Saved mfcc spectros')
-feature_extractor.save_mel_spectrograms(audio_files, sample_rate, '../data/train/mel_spectros/')
+feature_extractor.save_mel_spectrograms(
+    audio_files, sample_rate, './data/train/mel_spectros/')
 print('saved mel spectros')
 
 char_encoder = fit_label_encoder(transcripts)
@@ -51,26 +56,33 @@ print('model summary')
 model = model_3(char_encoder)
 print(model.summary())
 
-import math
 
-data_batch_size = 100
-training_batch_size = 25
-number_of_epochs = 100
+data_batch_size = 50
+training_batch_size = 12
+number_of_epochs = 1
 
 number_of_batches = math.ceil(len(mfcc_features)/data_batch_size)
 
 for i in range(number_of_epochs):
     for j in range(number_of_batches):
-        print('Epoch {}: training batch {}'.format(i+1,j))
-        X_train, y_train = load_spectrograms_with_transcripts_in_batches(mfcc_features, 
-                                                              enc_aug_transcripts, data_batch_size, j,
-                                                              '../data/train/mel_spectros/')
-        history = model.fit([X_train, y_train], batch_size = training_batch_size, epochs = 1)
+        print('Epoch {}: training batch {}'.format(i+1, j))
+        X_train, y_train = load_spectrograms_with_transcripts_in_batches(mfcc_features,
+                                                                         enc_aug_transcripts, data_batch_size, j,
+                                                                         './data/train/mel_spectros/')
+        history = model.fit([X_train, y_train],
+                            batch_size=training_batch_size, epochs=1)
 # with mlflow.start_run() as run:
 #     mlflow.log_metric("ctc_loss", history.history['loss'][-1])
 
-predicted = model.predict([X_train,y_train])
+predicted = model.predict([X_train, y_train])
 predicted_trans = decode_predicted(predicted, char_encoder)
 real_trans = [''.join(char_encoder.inverse_transform(y)) for y in y_train]
-print("pridicted:",predicted_trans[0])
-print("actual:",real_trans[0])
+print("pridicted:", predicted_trans[0])
+print("actual:", real_trans[0])
+
+with open("metrics.json", 'w') as outfile:
+    json.dump({"WER": 0.2}, outfile)
+
+with open("prediction.txt", 'w') as outfile:
+    outfile.write("Predicted: {}".format(predicted_trans[0]))
+    outfile.write("Actual: {}".format(real_trans[0]))
