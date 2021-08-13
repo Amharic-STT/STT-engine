5b2374c5f2b2c3ed0f695718d9e0f1521524428e
Yosef Alemneh <yosefworku67@gmail.com>
2021-08-13 05:01:42 +0300

    added more logging

--- scripts/report.py
+++ scripts/report.py
@@ -1,6 +1,3 @@
 
 with open('results.txt', 'w') as result:
       result.write('CML Report for Amharic Speech To Text Model\n')
-      result.write('Training CTC loss: 32.4 \n')
-      result.write('Validation CTC loss: 46.5 \n')
-      
